2f2861e6752e59a7159e165ca7f165a45bfa5fd7
hanifahreza <mhanifahreza@gmail.com>
2023-09-21 14:31:50 +0700

    Modify the UI of TransliteratePage and OCRPage

---
+++ src/componentPage/OCRPage/VariantSelect.js
@@ -0,0 +1,44 @@
+import {
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  VStack,
+  Text,
+} from "@chakra-ui/react";
+import { FaChevronDown } from "react-icons/fa";
+
+export const VariantSelect = ({ value, options, onChange }) => {
+  if (options.length === 0) {
+    return null;
+  }
+
+  return (
+    <VStack align="flex-start">
+      <Text fontSize="sm" textColor="gray.500">
+        Script
+      </Text>
+      <Menu>
+        <MenuButton
+          size="sm"
+          as={Button}
+          variant="outline"
+          fontWeight="normal"
+          gap={3}
+          ml={4}
+          rightIcon={<FaChevronDown />}
+        >
+          {value}
+        </MenuButton>
+        <MenuList>
+          {options.map((option) => (
+            <MenuItem key={option} onClick={onChange}>
+              {option}
+            </MenuItem>
+          ))}
+        </MenuList>
+      </Menu>
+    </VStack>
+  );
+};
