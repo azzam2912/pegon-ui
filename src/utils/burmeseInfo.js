a3790785401c3046695c4218349b8911fc0da8da
matthewsolomo <87316023+matthewsolomo@users.noreply.github.com>
2023-09-28 13:27:23 +0700

    Adding mon, burmese, and sgawkaren

---
+++ src/utils/burmeseInfo.js
@@ -0,0 +1,84 @@
+export const cheatSheetBurmeseConsonant = [
+    { name: "Basic Ka", burmese: "က", latin: "ka", standard: "ka"},
+    { name: "Basic Kha", burmese: "ခ", latin: "kha", standard: "kha" },
+    { name: "Basic Ga", burmese: "ဂ", latin: "ga", standard: "ga" },
+    { name: "Basic Gha", burmese: "ဃ", latin: "gha", standard: "gha" },
+    { name: "Basic Nga", burmese: "င", latin: "nga", standard: "nga" },
+    { name: "Basic Ca", burmese: "စ", latin: "ca", standard: "ca" },
+    { name: "Basic Cha", burmese: "ဆ", latin: "cha", standard: "cha" },
+    { name: "Basic Ja", burmese: "ဇ", latin: "ja", standard: "ja" },
+    { name: "Basic Jha", burmese: "ဈ", latin: "jha", standard: "jha" },
+    { name: "Basic Nnya", burmese: "ည", latin: "nnya", standard: "nnya" },
+    { name: "Basic Nya", burmese: "ဉ", latin: "nya", standard: "nya" },
+    { name: "Basic Tta", burmese: "ဋ", latin: "tta", standard: "tta" },
+    { name: "Basic Ttha", burmese: "ဌ", latin: "ttha", standard: "ttha" },
+    { name: "Basic Dda", burmese: "ဍ", latin: "dda", standard: "dda" },
+    { name: "Basic Ddha", burmese: "ဎ", latin: "ddha", standard: "ddha" },
+    { name: "Basic Nna", burmese: "ဏ", latin: "nna", standard: "nna" },
+    { name: "Basic Ta", burmese: "တ", latin: "ta", standard: "ta" },
+    { name: "Basic Tha", burmese: "ထ", latin: "tha", standard: "tha" },
+    { name: "Basic Da", burmese: "ဒ", latin: "da", standard: "da" },
+    { name: "Basic Dha", burmese: "ဓ", latin: "dha", standard: "dha" },
+    { name: "Basic Na", burmese: "န", latin: "na", standard: "na" },
+    { name: "Basic Pa", burmese: "ပ", latin: "pa", standard: "pa" },
+    { name: "Basic Pha", burmese: "ဖ", latin: "pha", standard: "pha" },
+    { name: "Basic Ba", burmese: "ဗ", latin: "ba", standard: "ba" },
+    { name: "Basic Bha", burmese: "ဘ", latin: "bha", standard: "bha" },
+    { name: "Basic Ma", burmese: "မ", latin: "ma", standard: "ma" },
+    { name: "Basic Ya", burmese: "ယ", latin: "ya", standard: "ya" },
+    { name: "Basic Ra", burmese: "ရ", latin: "ra", standard: "ra" },
+    { name: "Basic La", burmese: "လ", latin: "la", standard: "la" },
+    { name: "Basic Wa", burmese: "ဝ", latin: "wa", standard: "wa" },
+    { name: "Basic Sa", burmese: "သ", latin: "sa", standard: "sa" },
+    { name: "Basic Ssa", burmese: "ဿ", latin: "ssa", standard: "ssa" },
+    { name: "Basic Ha", burmese: "ဟ", latin: "ha", standard: "ha" },
+    { name: "Basic Lla", burmese: "ဠ", latin: "lla", standard: "lla" },
+    { name: "Basic A", burmese: "အ", latin: "a", standard: "a" }
+]
+
+export const cheatSheetBurmeseIndependentVowels = [
+    { name: "Independent I", burmese: "ဣ", latin: "i", standard: "i" },
+    { name: "Independent Ii", burmese: "ဤ", latin: "ii", standard: "ii" },
+    { name: "Independent U", burmese: "ဥ", latin: "u", standard: "u" },
+    { name: "Independent Uu", burmese: "ဦ", latin: "uu", standard: "uu" },
+    { name: "Independent E", burmese: "ဧ", latin: "e", standard: "e" },
+    { name: "Independent O", burmese: "ဩ", latin: "o", standard: "o" },
+    { name: "Independent Oˆ", burmese: "ဪ", latin: "oˆ", standard: "oˆ" }
+]
+
+export const cheatSheetBurmeseDependentVowels = [
+    { name: "Aa", burmese: "ာ", latin: "aa", standard: "aa" },
+    { name: "I", burmese: "ိ", latin: "i", standard: "i" },
+    { name: "Ii", burmese: "ီ", latin: "ii", standard: "ii" },
+    { name: "U", burmese: "ု", latin: "u", standard: "u" },
+    { name: "Uu", burmese: "ူ", latin: "uu", standard: "uu" },
+    { name: "E", burmese: "ေ", latin: "e", standard: "e" },
+    { name: "Ai", burmese: "ဲ", latin: "ai", standard: "ai" },
+    { name: "O", burmese: "ော", latin: "o", standard: "o" },
+    { name: "Oˆ", burmese: "ော်", latin: "oˆ", standard: "oˆ" },
+    { name: "Ui", burmese: "ို", latin: "ui", standard: "ui" }
+]
+
+export const cheatSheetBurmeseMedialConsonants = [
+    { name: "Medial y", burmese: "ျ", latin: "y", standard: "y" },
+    { name: "Medial w", burmese: "ွ", latin: "w", standard: "w" },
+    { name: "Medial r", burmese: "ြ", latin: "r", standard: "r" },
+    { name: "Medial h", burmese: "ှ", latin: "h", standard: "h" }
+]
+
+export const cheatSheetBurmeseFinalDiacritics = [
+    { name: "Asat", burmese: "်", latin: "asat", standard: "asat" },
+    { name: "Virama", burmese: "္", latin: "virama", standard: "virama" }
+]
+
+export const cheatSheetBurmeseTones = [
+    { name: "Low Tone", burmese: "့", latin: "'", standard: "'" },
+    { name: "High Tone", burmese: "း", latin: '"', standard: '"' }
+]
+
+
+export const cheatSheetBurmesePunctuations = [
+    { name: "Coma", burmese: "၊", latin: ",", standard: "," },
+    { name: "Period", burmese: "။", latin: ".", standard: "." }
+]
+
