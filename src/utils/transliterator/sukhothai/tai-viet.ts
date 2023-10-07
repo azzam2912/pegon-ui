62c1c72f0bdbb1c8353d30686721a4827092bdd2
matthewsolomo <87316023+matthewsolomo@users.noreply.github.com>
2023-10-07 20:38:38 +0700

    Update tai-viet.ts

--- src/utils/transliterator/sukhothai/tai-viet.ts
+++ src/utils/transliterator/sukhothai/tai-viet.ts
@@ -181,6 +181,8 @@ const MonographConsonants: PlainRule[] = [
   ["f\\", TaiViet.FoL],
   ["m/", TaiViet.MoH],
   ["m\\", TaiViet.MoL],
+  ["y/", TaiViet.YoH],
+  ["y\\", TaiViet.YoL],
   ["r/", TaiViet.RoH],
   ["r\\", TaiViet.RoL],
   ["l/", TaiViet.LoH],
