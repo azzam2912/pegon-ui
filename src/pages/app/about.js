ca913ae6bdce5fd474d339a9fab695ee094ab159
hanifahreza <mhanifahreza@gmail.com>
2023-10-08 08:17:03 +0700

    delete unnecessary files

--- src/pages/app/about.js
+++
@@ -1,6 +0,0 @@
-import { Page } from "src/componentPage/Page";
-import TutorialPage from "../../componentPage/TutorialPage";
-
-export default function FAQ() {
-  return <Page pageComponent={FAQPage} />;
-}
