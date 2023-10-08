f3cecdc57f0f9ab39a5e7d36e50b3890527f659f
hanifahreza <mhanifahreza@gmail.com>
2023-10-08 11:06:50 +0700

    Change ocr endpoint variable name

--- src/utils/ocrApi.js
+++ src/utils/ocrApi.js
@@ -1,8 +1,8 @@
 // api.js
 let apiEndpoint;
 
-if (process.env.NEXT_PUBLIC_OCR_API_HOST) {
-  apiEndpoint = process.env.NEXT_PUBLIC_OCR_API_HOST;
+if (process.env.LOCAL_OCR_API_HOST) {
+  apiEndpoint = process.env.LOCAL_OCR_API_HOST;
 } else {
   apiEndpoint = '' 
 }
